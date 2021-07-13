da984455644bdfe8b2954b537d84f911afcf168b
bthank <bthankachan@yahoo.com>
2021-07-13 19:15:34 -0400

    additional changes for page size and changing default resource path

--- src/main/java/com/binu/springboot/employeecrud/dao/EmployeeRepository.java
+++ src/main/java/com/binu/springboot/employeecrud/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.binu.springboot.employeecrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.binu.springboot.employeecrud.entity.Employee;
 
+// use @RepositoryRestResource annotation to change rest endpoint paths to use 'members' instead of 'employees'
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
